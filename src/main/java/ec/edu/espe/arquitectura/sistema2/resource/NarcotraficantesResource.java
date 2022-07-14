2cc3683d1b9704b107883ee1c360bf3c5a62909f
SheenSTVZ <jhoandange@gmail.com>
2022-07-13 21:19:12 -0500

    first commit

--- src/main/java/ec/edu/espe/arquitectura/sistema2/resource/NarcotraficantesResource.java
+++ src/main/java/ec/edu/espe/arquitectura/sistema2/resource/NarcotraficantesResource.java
@@ -3,6 +3,7 @@ package ec.edu.espe.arquitectura.sistema2.resource;
 import ec.edu.espe.arquitectura.sistema2.model.Narcotraficantes;
 import ec.edu.espe.arquitectura.sistema2.service.NarcotraficantesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,9 @@ public class NarcotraficantesResource {
     private final NarcotraficantesService narcotraficantesService;
 
     @GetMapping(path = "/{nombreCompleto}")
-    public Narcotraficantes findByNombreCompleto(String nombreCompleto) {
-        return narcotraficantesService.obtenerNarcotraficantePorNombreCompleto(nombreCompleto);
+    public ResponseEntity<String> findByNombreCompleto(String nombreCompleto) {
+        return ResponseEntity.ok(narcotraficantesService
+                .obtenerNarcotraficantePorNombreCompleto(nombreCompleto)
+                .getSancionado());
     }
 }
